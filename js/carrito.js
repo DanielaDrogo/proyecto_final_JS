26182f98eea9e57dca4e17fb63a457393c39f5ce
Daniela Drogo <danieladrogo@hotmail.com>
2024-11-01 15:28:00 -0300

    hice correciones en el codigo

--- js/carrito.js
+++ js/carrito.js
@@ -1,65 +1,42 @@
 // ~CARRITO~ //
 
-document.querySelectorAll('.btn.btn-success').forEach((button, index) => {
-    button.addEventListener('click', () => {
-
-        // si existe en el producto en el carrito, aumento la cantidad
-        if (carrito.includes(productos[index])) {
-            productos[index].cantidad++
-        } else {
-            productos[index].cantidad++
-            carrito.push(productos[index]);
-        }
-
-        console.log(`${productos[index].nombre} ha sido añadido al carrito.`);
-        actualizarCarrito();
-        guardarCarrito();
-    });
-});
+let carrito = [];
 
 function actualizarCarrito() {
     let listaCarrito = document.querySelector('.listaCarrito');
-    let totalCarrito = document.querySelector('.totalCarrito')
-    
+    let totalCarrito = document.querySelector('.totalCarrito');
     let total = carrito.reduce((acumulador, valorActual) => acumulador + (valorActual.precio * valorActual.cantidad), 0);
-    
+
     listaCarrito.innerHTML = '';
     totalCarrito.innerHTML = '';
-
     if (carrito.length === 0) {
         listaCarrito.innerHTML = '<h6>Compra realizada con exito...</h6>';
     } else {
         carrito.forEach((producto) => {
             listaCarrito.innerHTML += `
-            <div class="productoCargado">
-                <p>${producto.nombre} - $${producto.precio} - ${producto.cantidad}</p>
-                <button type="button" class="btn btn-link" value="${producto.nombre}">Eliminar</button>
-            </div>    
-            `;
+                <div class="productoCargado">
+                    <p>${producto.nombre} - $${producto.precio} - ${producto.cantidad}</p>
+                    <button type="button" class="btn btn-link" value="${producto.nombre}">Eliminar</button>
+                </div>
+                `;
         });
-
-        let botones_eliminar = document.querySelectorAll('.btn.btn-link')
+        let botones_eliminar = document.querySelectorAll('.btn.btn-link');
         botones_eliminar.forEach(botonEliminar => {
             botonEliminar.addEventListener('click', () => {
-
                 let productoEliminar = carrito.find(produc => produc.nombre === botonEliminar.value);
                 let indice = carrito.indexOf(productoEliminar);
-
                 if (carrito[indice].cantidad > 1) {
-                    carrito[indice].cantidad--
+                    carrito[indice].cantidad--;
                 } else {
                     carrito = carrito.filter((productos) => productos.nombre != botonEliminar.value);
                 }
                 actualizarCarrito();
-                guardarCarrito()
-
-            })
-        })
-
+                guardarCarrito();
+            });
+        });
         totalCarrito.innerHTML = `Total: $${total}`;
     }
 }
-
 
 function guardarCarrito() {
     localStorage.setItem('carrito', JSON.stringify(carrito));
@@ -73,7 +50,9 @@ function cargarCarrito() {
     }
 }
 
-cargarCarrito(); 
+cargarCarrito();
+
+document.querySelector('#btn_comprar').addEventListener('click', comprar);
 
 function comprar() {
     localStorage.clear();
@@ -81,22 +60,3 @@ function comprar() {
     actualizarCarrito();
     console.log('la compra fue realizada con exito');
 }
-document.querySelector('#btn_comprar').addEventListener('click', comprar);
-
-
-let botonAgregarCarrito = document.querySelectorAll('.btn.btn-success');
-botonAgregarCarrito.forEach((button) => {
-    button.addEventListener('click', () => {
-
-        Toastify({
-            text: "Producto añadido al carrito",
-            gravity: "bottom",
-            duration: 1500,
-            close: true,
-            style: {
-                background: "red",
-            }
-
-        }).showToast();
-    });
-})
--- js/productos.js
+++ js/productos.js
@@ -6,24 +6,23 @@ fetch('data.json')
         let contenedorDeTarjetas = document.querySelector('.contenedor-card2');
         datos.forEach((producto, index) => {
             contenedorDeTarjetas.innerHTML += `
-            <div class="contenedor-card2">
-                <div class="card" style="width: 18rem;">
-                    <img src="./imagenes/${producto.imagen}" class="card-img-top" alt="imagen">
-                    <div class="card-body">
-                        <h5 class="card-title">${producto.nombre}</h5>
-                        <p class="card-text">$ ${producto.precio}</p>
-                        <a class="btn btn-success" data-index="${index}">Añadir al carrito 🛒</a>
+                <div class="contenedor-card2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="./imagenes/${producto.imagen}" class="card-img-top" alt="imagen">
+                        <div class="card-body">
+                            <h5 class="card-title">${producto.nombre}</h5>
+                            <p class="card-text">$ ${producto.precio}</p>
+                            <a class="btn btn-success" data-index="${index}">Añadir al carrito </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            `;
+                `;
         });
 
         document.querySelectorAll('.btn.btn-success').forEach((button) => {
             button.addEventListener('click', () => {
                 const index = button.getAttribute('data-index');
                 const producto = datos[index];
-
                 const encontrado = carrito.find(item => item.nombre === producto.nombre);
                 if (encontrado) {
                     encontrado.cantidad++;
@@ -33,7 +32,6 @@ fetch('data.json')
                 }
                 actualizarCarrito();
                 guardarCarrito();
-
                 Toastify({
                     text: "Producto añadido al carrito",
                     gravity: "bottom",
@@ -47,5 +45,3 @@ fetch('data.json')
         });
     })
     .catch((error) => console.log(error));
-
-    
